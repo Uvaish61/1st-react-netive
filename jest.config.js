a7f4c2f96ca90ffa303d3fef7060c1e3528d39c4
Uvaish61 <uvaishkhanwork@gmail.com>
2026-04-08 23:12:03 +0530

    update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native|@react-navigation)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native|@react-navigation|nativewind|react-native-css-interop|react-native-vector-icons)/)',
   ],
 };
